90505beb0c93c415e37112968bf9f141f33d7af5
Paul Moore <p.f.moore@gmail.com>
2016-08-09 15:39:01 +0100

    First working version of newstub

--- experimental/newstub.cpp
+++ experimental/newstub.cpp
@@ -29,6 +29,7 @@ debug(char *format, ...)
 
 void print_w(std::wstring s) {
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    s = s + L"\n";
     WriteConsoleW(consoleHandle, s.c_str(), s.length(), NULL, NULL); 
 }
 
@@ -134,7 +135,7 @@ struct LineGen
     LineGen(std::wstring data) : data_(data), pos_(0) {}
     std::wstring next() {
         const auto NL = L"\r\n";
-        if (pos_ == std::wstring::npos)
+        if (finished())
             return std::wstring();
         auto end = data_.find_first_of(NL, pos_);
         auto ret = data_.substr(pos_, end-pos_);
@@ -142,6 +143,10 @@ struct LineGen
             end = data_.find_first_not_of(NL, end);
         pos_ = end;
         return ret;
+    }
+
+    bool finished() {
+        return pos_ == std::wstring::npos;
     }
 
     private:
@@ -233,18 +238,166 @@ std::wstring load_appended_config()
     return config;
 }
 
+void set_cwd(std::wstring dir) {
+    if (!SetCurrentDirectoryW(dir.c_str()))
+        error(L"Couldn't set directory to %ls", dir.c_str());
+}
+
+void set_env(std::wstring name, std::wstring value) {
+    if (!SetEnvironmentVariableW(name.c_str(), value.c_str()))
+        error(L"Couldn't set %ls to %ls", name.c_str(), value.c_str());
+}
+
+void add_path(std::wstring entry) {
+
+}
+
+/*
+ * Copyright (C) 2011-2013 Vinay Sajip.
+ * Licensed to PSF under a contributor agreement.
+ *
+ * Based on the work of:
+ *
+ * Mark Hammond (original author of Python version)
+ * Curt Hagenlocher (job management)
+ */
+
+// #include <shlobj.h>
+
+/*
+ * Process creation code
+ */
+
+static BOOL
+safe_duplicate_handle(HANDLE in, HANDLE * pout)
+{
+    BOOL ok;
+    HANDLE process = GetCurrentProcess();
+    DWORD rc;
+
+    *pout = NULL;
+    ok = DuplicateHandle(process, in, process, pout, 0, TRUE,
+                         DUPLICATE_SAME_ACCESS);
+    if (!ok) {
+        rc = GetLastError();
+        if (rc == ERROR_INVALID_HANDLE) {
+            debug("DuplicateHandle returned ERROR_INVALID_HANDLE\n");
+            ok = TRUE;
+        }
+        else {
+            debug("DuplicateHandle returned %d\n", rc);
+        }
+    }
+    return ok;
+}
+
+static BOOL WINAPI
+ctrl_c_handler(DWORD code)
+{
+    return TRUE;    /* We just ignore all control events. */
+}
+
+static void
+run_child(std::wstring cmd) {
+    wchar_t * cmdline = const_cast<wchar_t*>(cmd.c_str());
+    HANDLE job;
+    JOBOBJECT_EXTENDED_LIMIT_INFORMATION info;
+    DWORD rc;
+    BOOL ok;
+    STARTUPINFOW si;
+    PROCESS_INFORMATION pi;
+    print_w(cmd);
+
+#if defined(_WINDOWS)
+    // When explorer launches a Windows (GUI) application, it displays
+    // the "app starting" (the "pointer + hourglass") cursor for a number
+    // of seconds, or until the app does something UI-ish (eg, creating a
+    // window, or fetching a message).  As this launcher doesn't do this
+    // directly, that cursor remains even after the child process does these
+    // things.  We avoid that by doing a simple post+get message.
+    // See http://bugs.python.org/issue17290 and
+    // https://bitbucket.org/vinay.sajip/pylauncher/issue/20/busy-cursor-for-a-long-time-when-running
+    MSG msg;
+
+    PostMessage(0, 0, 0, 0);
+    GetMessage(&msg, 0, 0, 0);
+#endif
+
+    debug("run_child: about to run '%s'\n", cmdline);
+    job = CreateJobObject(NULL, NULL);
+    ok = QueryInformationJobObject(job, JobObjectExtendedLimitInformation,
+                                  &info, sizeof(info), &rc);
+    if (!ok || (rc != sizeof(info)) || !job)
+        error(L"Job information querying failed");
+    info.BasicLimitInformation.LimitFlags |= JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE |
+                                             JOB_OBJECT_LIMIT_SILENT_BREAKAWAY_OK;
+    ok = SetInformationJobObject(job, JobObjectExtendedLimitInformation, &info,
+                                 sizeof(info));
+    if (!ok)
+        error(L"Job information setting failed");
+    memset(&si, 0, sizeof(si));
+    si.cb = sizeof(si);
+    ok = safe_duplicate_handle(GetStdHandle(STD_INPUT_HANDLE), &si.hStdInput);
+    if (!ok)
+        error(L"stdin duplication failed");
+    ok = safe_duplicate_handle(GetStdHandle(STD_OUTPUT_HANDLE), &si.hStdOutput);
+    if (!ok)
+        error(L"stdout duplication failed");
+    ok = safe_duplicate_handle(GetStdHandle(STD_ERROR_HANDLE), &si.hStdError);
+    if (!ok)
+        error(L"stderr duplication failed");
+
+    ok = SetConsoleCtrlHandler(ctrl_c_handler, TRUE);
+    if (!ok)
+        error(L"control handler setting failed");
+
+    si.dwFlags = STARTF_USESTDHANDLES;
+    ok = CreateProcessW(NULL, cmdline, NULL, NULL, TRUE,
+                        0, NULL, NULL, &si, &pi);
+    if (!ok) {
+        /* TODO: Fix up - cmdline is a wchar*! */
+        error(L"Unable to create process using '%ls'", cmdline);
+    }
+    AssignProcessToJobObject(job, pi.hProcess);
+    CloseHandle(pi.hThread);
+    WaitForSingleObjectEx(pi.hProcess, INFINITE, FALSE);
+    ok = GetExitCodeProcess(pi.hProcess, &rc);
+    if (!ok)
+        error(L"Failed to get exit code of process");
+    debug("child process exit code: %d\n", rc);
+    exit(rc);
+}
 
 int main(int argc, char *argv[])
 {
     std::wstring config = load_appended_config();
     auto lines = LineGen(config);
-    std::wstring line;
-    while (line = lines.next(), line.length() > 0) {
-        if (*(line.begin()) == L'[' && *(line.end()-1) == L']') {
-            print_w(std::wstring(L"Section: ") + line.substr(1, line.length()-2) + L"\n");
-        } else {
-            auto p = split(line, L'=');
-            print_w(std::wstring(L"Data: ") + strip(p.first) + L" -> " + strip(p.second) + L"\n");
+    std::wstring section;
+    std::wstring executable;
+    while (!lines.finished()) {
+        auto line = lines.next();
+        if (line.front() == L'[' && line.back() == L']') {
+            section = line.substr(1, line.length()-2);
+            continue;
+        }
+        auto p = split(line, L'=');
+        auto key = strip(p.first);
+        auto value = strip(p.second);
+        print_w(section);
+        print_w(key);
+        print_w(value);
+        if (section == L"shim") {
+            if (key == L"executable") {
+                debug("executable is %ls\n", value.c_str());
+                executable = value;
+            }
+            else if (key == L"cwd")
+                set_cwd(value);
+        } else if (section == L"environment") {
+            set_env(key, value);
+        } else if (section == L"path") {
+            add_path(key);
         }
     }
+    run_child(executable);
 }
